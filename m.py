05f4678d2653f7d80f404ba3c378ef47db6bad28
Nawaf2429 <nawafaldebasi2@gmail.com>
2025-09-15 14:39:05 +0300

    Add files via upload

---
+++ m.py
@@ -0,0 +1,72 @@
+text = "   welcome to PYTHON programming 101!   "
+
+print(text.strip())
+
+print(text.capitalize())
+
+print(text.upper())
+
+print(text.lower())
+
+print(text.title())
+
+print(text.swapcase())
+
+print(text.center(50, '-'))
+
+print(text.count("o"))
+
+print(text.replace("PYTHON", "Java"))
+
+print(text.split())
+
+print(text.join("|"))
+
+#
+
+print(text.startswith("welcome"))
+
+print(text.endswith("101!"))
+
+print(text.isalnum())
+
+print(text.isalpha())
+
+print(text.islower())
+
+print(text.isupper())
+
+print(text.isspace())
+
+#
+
+print("cat" == "cat")
+
+print("cat" != "Cat")
+
+print("cat" < "caterpillar")
+
+print("dog" > "Dog")
+
+#print("10" == 10, "10" != 10 and "10" > 10)
+
+#
+
+lis=["dog", "cat", "zebra", "ant"]
+print(sorted(lis))
+
+print(lis.sort())
+
+
+
+#
+
+y=1
+print(str(y))
+
+t='10'
+print(int(y))
+
+r=1
+print(float())
+print(int())
